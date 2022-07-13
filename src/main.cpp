dbbb602b6b982b0ac078e8535d5c0f962131a3f7
John Roughley <jroughley@softwareinstitute.com>
2022-07-13 15:35:40 +0100

    completed leetcode exercise 48

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/rotate-image/
+
 #include<iostream>
 #include"../inc/solution.hpp"
 
@@ -8,15 +10,18 @@ void PrintMatrix(const std::vector<std::vector<int>>& matrix)
 	{
 		for(auto element : row)
 		{
-			std::cout << element;
+			std::cout << element << ' ';
 		}
-		std::cout << 'n';
+		std::cout << '\n';
 	}
-}
+} 
 
 int main()
 {
+	//std::vector<std::vector<int>> matrix{{1}};
+	//std::vector<std::vector<int>> matrix{{1,2},{3,4}};
 	std::vector<std::vector<int>> matrix{{1,2,3},{4,5,6},{7,8,9}};
+	//std::vector<std::vector<int>> matrix{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
 
 	Solution sol{};
 	sol.rotate(matrix);
--- src/solution.cpp
+++ src/solution.cpp
@@ -1,7 +1,24 @@
 #include"../inc/solution.hpp"
 
 
+// 90 deg CW rotation of elements == matrix transpose then reflection about its vertical axis
 void Solution::rotate(std::vector<std::vector<int>>& matrix)
-{
-    
+{  
+    int size = matrix.size();
+    // matrix transpose, iterate over either upper/lower triangle elements only
+    for(int row{0}; row < size; ++row)
+    {
+        for(int col{0}; col < row; ++col) // only iterates over lower triangle elements
+        {
+            std::swap(matrix[row][col], matrix[col][row]);
+        }
+    }
+    // reflection of matrix about its vertical axis
+    for(int row{0}; row < size; ++row)
+    {
+        for(int col{0}; col < size/2; ++col) // only iterate over columns up to the reflection axis
+        {
+            std::swap(matrix[row][col], matrix[row][size-(col+1)]); // col 0 <--> col size-1, col 1 <--> col size-2 etc.
+        }
+    }
 }
